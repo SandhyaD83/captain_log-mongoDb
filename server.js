8ddc777526da63256bc62d7dfb21cb9ba6099225
“SandhyaD83” <sandhyadandamudi@gmail.com>
2023-04-07 11:42:05 -0400

    conneted to MongoDb and created Log

--- server.js
+++ server.js
@@ -1,4 +1,8 @@
-const express = require('express')
+require('dotenv').config()
+const express = require('express');
+const mongoose = require('mongoose');
+
+// const Log = require('./models/logs');
 const app = express();
 const port = 3000;
 app.set('view engine', 'jsx');
@@ -8,9 +12,21 @@ app.use((req, res, next) => {
     console.log('I run for all routes');
     next();
 })
+mongoose.set('strictQuery', true);
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+mongoose.connection.once('open', () => {
+    console.log('connected to mongo');
+});
 app.get('/logs', (req, res) => {
     res.send('new')
 })
+app.get('/log/:id', (req, res) => {
+    res.render('Show')
+})
+
 app.get('/logs/new', (req, res) => {
     res.render('New')
 })
@@ -21,7 +37,10 @@ app.post('/logs', (req, res) => {
     else {
         req.body.shipIsBroken = false
     }
-    res.send(req.body)
+    Log.create(req.body, (err, createdLog) => {
+        res.redirect('/logs/:id')
+    })
+    // res.send(req.body)
 })
 app.listen(port, () => {
     console.log(`Listening to port: ${port} `)
---
+++ views/Show.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+
+export default class Show extends Component {
+    render() {
+        return (
+            <div>Show</div>
+        )
+    }
+}
+module.exports = Show
