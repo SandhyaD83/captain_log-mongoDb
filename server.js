89b6292a5fb6bad0eaf6480b9c9cfc81de562586
“SandhyaD83” <sandhyadandamudi@gmail.com>
2023-04-07 10:23:37 -0400

    created a route for the post method and configured body-parser

--- server.js
+++ server.js
@@ -3,11 +3,25 @@ const app = express();
 const port = 3000;
 app.set('view engine', 'jsx');
 app.engine('jsx', require('express-react-views').createEngine())
+app.use(express.urlencoded({ extended: false }))
+app.use((req, res, next) => {
+    console.log('I run for all routes');
+    next();
+})
 app.get('/logs', (req, res) => {
     res.send('new')
 })
 app.get('/logs/new', (req, res) => {
     res.render('New')
+})
+app.post('/logs', (req, res) => {
+    if (req.body.shipIsBroken == 'on') {
+        req.body.shipIsBroken = true
+    }
+    else {
+        req.body.shipIsBroken = false
+    }
+    res.send(req.body)
 })
 app.listen(port, () => {
     console.log(`Listening to port: ${port} `)
